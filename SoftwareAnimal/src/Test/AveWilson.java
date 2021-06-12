e8d32712a62a4ec3144db913d7aca1488ff8b323
Dourado8083 <gustavo.dourado.br@gmail.com>
2021-06-12 10:30:02 -0300

    refiz pois deu erro

--- SoftwareAnimal/src/Test/AveWilson.java
+++ SoftwareAnimal/src/Test/AveWilson.java
@@ -1,4 +1,32 @@
 package Test;
 
-public class AveWilson {
+public class AveWilson extends Animal {
+public String velocidadeVo;
+public String  tamanhoBico;
+
+public AveWilson(String especie, String nomeCientífico,
+		Double pesoMedio, String classe, String longevidade,
+		String origem, String curiosidade, String habilidade) {
+	super(especie, nomeCientífico, pesoMedio, classe, longevidade, origem);
+	
+	this.velocidadeVo = velocidadeVo;
+	this.tamanhoBico = tamanhoBico;	
+}
+
+public String getVelocidadeVo() {
+	return velocidadeVo;
+}
+
+public void setVelocidadeVo(String velocidadeVo) {
+	this.velocidadeVo = velocidadeVo;
+}
+
+public String getTamanhoBico() {
+	return tamanhoBico;
+}
+
+public void setTamanhoBico(String tamanhoBico) {
+	this.tamanhoBico = tamanhoBico;
+}
+
 }
